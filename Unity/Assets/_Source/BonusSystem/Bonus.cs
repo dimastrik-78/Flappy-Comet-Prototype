230cc53b57022af3adfd9374626d48c6e9491b10
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-09-05 15:30:10 +0300

    Take bonus

---
+++ Unity/Assets/_Source/BonusSystem/Bonus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using Utils;
+using Utils.Event;
+
+namespace BonusSystem
+{
+    public class Bonus : MonoBehaviour
+    {
+        [SerializeField] private LayerMask player;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (player.Contains(other.gameObject.layer))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
